913ea1ca1ad0c76ff70fcd5aeefe1c24f32219ae
Bruno Jullien <bruno.jullien@gmail.com>
2022-04-01 19:39:04 -0700

    4.1. Conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.Fragment>
+    <App authorized={true} />,
+    <Footer />
+  </React.Fragment>,
+  document.getElementById("root")
+);
+
+function Footer() {
+  return <h6>Copyright {new Date().getFullYear()}</h6>
+}
