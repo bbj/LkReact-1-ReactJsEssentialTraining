975b22a151617961dc245f6ca833f45952c1a963
Bruno Jullien <bruno.jullien@gmail.com>
2022-04-05 10:55:12 -0700

    4.5. Incorporating useReducer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState('happy');
-  const [secondary, setSecondary] = useState('tired');
-
-  // used to implement side-effect related to component render, not rendering itself
-  // generally to console log, fetch data ...
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-  //called the 'dependency array', if empty, useEffect() called 1 time at first render.
-  // [emotion]: useEffect() called every time emotion state changes
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
-
+  const [checked, toggle] = useReducer(
+    (checked) => !checked, //reducer: take current state and return new state
+    false
+  );
 
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}.</h1>
-      <button onClick={() => setEmotion('happy')}>
-        Make Happy
-      </button>
-      <button onClick={() => setSecondary('crabby')}>
-        Make Crabby
-      </button>
-      <button onClick={() => setEmotion('frustrated')}>
-        Frustrate
-      </button>
-      <button onClick={() => setEmotion('enthusiastic')}>
-        Enthuse
-      </button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={toggle} />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
