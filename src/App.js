7d15169e8cd3ace41e4d0a035a90f890fb286842
Bruno Jullien <bruno.jullien@gmail.com>
2022-04-06 14:48:20 -0700

    5.3. Handling loading states

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import "./App.css";
 
 function App({ login }) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/uers/${login}`)
       .then((response) => response.json())
-      .then(setData);
-  }, []);
+      .then(setData)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(setError);            //(error) => { setError(error) }
+  }, [login]);                    //call useEffect every time login changes
 
-  if (data) {
-    return (<div>
-      <h1>{data.name}</h1>
-      <p>{data.location}</p>
-      <img alt={data.login} src={data.avatar_url} width="200px" />
-    </div>);
-  }
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
-  return <div>No user available.</div>
+  return (<div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img alt={data.login} src={data.avatar_url} width="200px" />
+  </div>);
 }
 
 export default App;
